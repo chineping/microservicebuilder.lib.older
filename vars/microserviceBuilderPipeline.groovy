dfab044bfeeb2e5150b84f3d7bb1a4dd444bde1f
andonguyen <do.nguyen@gmail.com>
2018-04-16 14:18:51 +1000

    adding bx plugin

--- vars/microserviceBuilderPipeline.groovy
+++ vars/microserviceBuilderPipeline.groovy
@@ -197,6 +197,7 @@ def call(body) {
           
           container ('helm') {
             sh "/helm init --client-only --skip-refresh"
+            sh "bx plugin list"
             def deployCommand = "/helm install ${realChartFolder} --wait --set test=true --values pipeline.yaml --namespace ${testNamespace} --name ${tempHelmRelease} --tls"
             if (fileExists("chart/overrides.yaml")) {
               deployCommand += " --values chart/overrides.yaml"
